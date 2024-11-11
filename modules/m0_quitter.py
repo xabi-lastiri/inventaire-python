f1cfc43888d77c8fd2470124de2a8128b633344f
xabi-lastiri <xabi.dihartce@gmail.com>
2024-11-11 18:44:42 +0100

    MAJ.

--- modules/m0_quitter.py
+++ modules/m0_quitter.py
@@ -1,9 +1,9 @@
 def m0_quitter():
     while True:
-        n = input(f"\nConfirmez-vous vouloir quitter ? (o/n) \n\n").lower()
-        if n == "o":
+        confirmer = input(f"\nConfirmez-vous vouloir quitter ? (o/n) \n\n").lower()
+        if confirmer == "o":
             exit()
-        if n == "n":
+        if confirmer == "n":
             break
         else:
             print("\nValeur incorrecte. Veuillez re-saisir votre réponse.")
--- modules/m1_vendre.py
+++ modules/m1_vendre.py
@@ -1,14 +1,14 @@
 from library import *
 
 def m1_vendre(inventaire, registre, utilisateur):
-    while True:
-        # pour arrêter le programme que lorsque l'utilisateur le décide
+    while True: # pour arrêter le programme que lorsque l'utilisateur le décide
         while True:
             # demander à l'utilisateur de saisir la référence à vendre
-            # vérifier que la référence à vendre existe dans l'inventaire
             reference = input(f"\nSaisissez la référence à vendre ({f3_afficher_liste_references(inventaire)})\nPour annuler, laissez vide.\n\n")
+            # vérifier si l'utilisateur souhaite annuler l'opération
             if f0_valeur_saisie_vide(reference):
                 return
+            # vérifier que la référence à vendre existe dans l'inventaire et que le stock correspondant n'est pas nul
             if f1_reference_existe(reference, inventaire):
                 if inventaire[reference] == 0:
                     print(f"Il n'y a plus de {reference} en stock.")
@@ -17,10 +17,10 @@ def m1_vendre(inventaire, registre, utilisateur):
             else:
                 print(f"\n{reference} n'existe pas dans l'inventaire.")
         while True:
-            # demander à l'utilisateur de saisir la quantité à vendre
-            # vérifier que la quantité à vendre soit (1) cohérente et (2) possible compte tenu du stock diponible dans l'inventaire au moment de l'opération
             try:
+                # demander à l'utilisateur de saisir la quantité à vendre
                 quantite_a_vendre = int(input(f"\nIl reste {inventaire[reference]} unité(s) en stock pour le produit {reference}.\nSaisissez la quantité à vendre : \n\n"))
+                # vérifier que la quantité à vendre soit (1) cohérente et (2) possible compte tenu du stock diponible dans l'inventaire au moment de l'opération
                 if f2_verifier_valeur_entiere_positive(quantite_a_vendre) and quantite_a_vendre <= inventaire[reference]:
                     break
                 print("\nLe stock est insuffisant.")
@@ -33,7 +33,7 @@ def m1_vendre(inventaire, registre, utilisateur):
                 break
             if n == "o":
                 inventaire[reference] -= quantite_a_vendre
-                # enregistrement de l'opération dans le registre
+                # enregistrer l'opération dans le registre
                 registre.append(f"{utilisateur} : VENTE de {quantite_a_vendre} unité(s) de {reference}")
                 print(
                     f"\nVous venez de vendre {quantite_a_vendre} unité(s) de {reference}. Il reste maintenant {inventaire[reference]} unité(s) en stock")
